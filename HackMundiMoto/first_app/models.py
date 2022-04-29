3489e501b17a27aaefda4aaffdbd61e222c1ae68
GerardBurgues <gerard61999@gmail.com>
2022-04-29 23:55:57 +0200

    views

--- HackMundiMoto/first_app/models.py
+++ HackMundiMoto/first_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Contact(models.Model):
+
+    name =models.CharField(blank=True, max_length=250)
+    email = models.EmailField(blank=True)
+    message =models.CharField(blank=True, max_length=10000)
