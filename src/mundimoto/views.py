f57eec8243fc94aca17c849928a62048346d75b5
josepmdc <jdomingocatafal@gmail.com>
2022-04-30 13:44:47 +0200

    Render bikes on table

--- src/mundimoto/views.py
+++ src/mundimoto/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-from mundimoto.models import Brands
 from . import forms
-from mundimoto.forms import FindMotorbike
+from mundimoto.models import Brands, Versions
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
@@ -27,15 +26,11 @@ def index(request):
 
 
 def home(request):
-    print(Brands.objects.all()[0].name)
-
     return render(request, 'home.html')
 
+
 def Login(request):
-
-    return render(request, "Login.html")
-
-
+    return render(request, "login.html")
 
 
 def formBike(request):
@@ -45,7 +40,9 @@ def formBike(request):
         find_form = forms.FindMotorbike(data=request.POST)
     if find_form.is_valid():
         find_form.save()
-    return render(request, "home.html",{'form':find_form})
+    brands = Brands.objects.all()
+    bikes = Versions.objects.all()
+    return render(request, "home.html", {'form': find_form, 'brands': brands, 'bikes': bikes})
 
 
 def Register(request):
@@ -70,7 +67,6 @@ def Register(request):
         else:
             print("ERROR")
             messages.error(request, "Username already Exists")
-
 
     else:
         print(request)
